41619ca39ca385eab1321e714f1e1e72e4b3a91a
MarkP05 <mvpfister5@gmail.com>
2025-02-12 17:57:24 -0500

    Created basic calculator for adding whole numbers.

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter the first whole number");
+        int num1 = scan.nextInt();
+
+        System.out.println("Please enter the second whole number");
+        int num2 = scan.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum of the two numbers is: " + sum);
+
+        scan.close();
+    }
+}
